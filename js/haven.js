aa0d3b3ca773a44f6a47b89aa099735db2c6f2b3
One-True-Mission <Stevenhowell27@yahoo.com>
2025-09-29 15:16:27 -0500

    Delete js/haven.js

--- js/haven.js
+++
@@ -1,7 +0,0 @@
-// Quick Exit everywhere
-const qe = document.getElementById('quickExit');
-if (qe) qe.onclick = () => window.location.replace('https://www.google.com/');
-
-// Year in footer
-const y = document.getElementById('year');
-if (y) y.textContent = new Date().getFullYear();
